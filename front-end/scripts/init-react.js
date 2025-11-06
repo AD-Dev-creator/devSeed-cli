f78a4d15e509e240a19029ac84831f4761e513df
AD-Dev-creator <cante11.dev@outlook.fr>
2025-11-06 16:01:50 +0100

    scrypt new project react ok

--- front-end/scripts/init-react.js
+++ front-end/scripts/init-react.js
@@ -4,7 +4,38 @@ import path from "path";
 
 export function initReactProject(projectPath) {
   try {
-    // Add your code for create React project structure and dependencies here
+    const projectName = path
+      .basename(projectPath)
+      .toLowerCase()
+      .replace(/[^a-z0-9-_]/g, "");
+    const safeProjectPath = path.join(path.dirname(projectPath), projectName);
+
+    if (!fs.existsSync(safeProjectPath)) {
+      fs.mkdirSync(projectPath, { recursive: true });
+    }
+
+    execSync(`npx create-react-app ${safeProjectPath}`, {
+      stdio: "inherit",
+    });
+
+    execSync(`npm install axios react-router-dom`, {
+      cwd: safeProjectPath,
+      stdio: "inherit",
+    });
+
+    const folder = ["src/components", "src/pages"];
+
+    const files = [".env", ".env.example"];
+
+    folder.forEach((folder) => {
+      fs.mkdirSync(path.join(safeProjectPath, folder), { recursive: true });
+    });
+
+    files.forEach((file) => {
+      fs.writeFileSync(path.join(safeProjectPath, file), "");
+    });
+
+    console.log("✅ React project initialized successfully!");
   } catch (error) {
     console.error("❌ Error initializing React project:", error);
   }
